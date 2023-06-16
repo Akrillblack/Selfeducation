2b40986c9349c03f725c8f54a8d4f09263b93c7d
Donatas Vidutis <donatasvidutis@gmail.com>
2023-06-16 15:44:36 +0300

    LSN_1 Project creation

---
+++ LSN_1/LSN_1/LSN_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LSN_1
+//
+//  Created by Donatas on 16.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# Selfeducation
